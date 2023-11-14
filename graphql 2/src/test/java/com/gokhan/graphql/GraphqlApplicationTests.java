28bf267ee469b5e4493f176b147ede2a6c61a9aa
gkhn01dnmz <gokhan01donmez@gmail.com>
2023-11-14 08:50:05 +0300

    Spring boot ile Graphql project

---
+++ graphql 2/src/test/java/com/gokhan/graphql/GraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gokhan.graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

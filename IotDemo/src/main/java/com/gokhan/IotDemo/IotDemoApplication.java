332215cb972d416d04754532184bea6de1492587
gkhn01dnmz <gokhan01donmez@gmail.com>
2023-11-21 12:19:41 +0300

    Mqtt project

---
+++ IotDemo/src/main/java/com/gokhan/IotDemo/IotDemoApplication.java
@@ -0,0 +1,13 @@
+package com.gokhan.IotDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotDemoApplication.class, args);
+	}
+
+}
